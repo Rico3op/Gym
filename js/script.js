367029d6bd3fa425ffebbcf061ade0e27f89adca
Rico3op <oric74120@gmail.com>
2024-05-10 18:41:14 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,3 @@
-function sendMail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
-    };
-
-    const serviceID = "service_rjrbklj";
-    const templateID = "template_ooo31vj";
-
-
-    emailjs.send(serviceID, templateID, params)
-        .then(res => {
-            nameInput.value = "";
-            emailInput.value = "";
-            messageInput.value = "";
-            console.log(res);
-            alert("Your message was sent successfully!!");
-        })
-        .catch(err => console.log(err));
-
-}
 
 let navbar = document.querySelector('.header .navbar');
 let menu = document.querySelector('#menu');
@@ -32,4 +10,4 @@ menu.onclick = () => {
 window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
-}
+};
